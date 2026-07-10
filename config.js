4bc4610a52d59f821dcdbcfb77b17c9f72e0a336
SWAYGFX•STUDIOS® <swaggboyswayjay@live.com>
2026-07-10 13:02:45 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,11 @@
+export default {
+  pinterest: {
+    feedUrl: "PASTE_YOUR_RSS_APP_PINTEREST_FEED_HERE"
+  },
 
+  discord: {
+    webhook: process.env.DISCORD_WEBHOOK
+  },
+
+  botName: "📌 Pinterest Updates"
+};
